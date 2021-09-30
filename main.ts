5bc054543688b38d4e1e99ad546fe153ff6aa6ad
318511 <91246969+318511@users.noreply.github.com>
2021-09-30 16:38:02 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,77 @@
-let X = 0
 let Y = 0
-for (let index = 0; index < 2; index++) {
-	
+let X = 0
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . # . #
+        . # . # .
+        # . # . .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        # . # . .
+        . # . # .
+        . . # . #
+        . # . . .
+        `)
 }
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
-	
+    X = 0
+    Y = 0
+    for (let index = 0; index < 2; index++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plot(X, Y)
+            basic.pause(100)
+        }
+        Y += 1
+        for (let X = 0; X <= 4; X++) {
+            led.plot(4 - X, Y)
+            basic.pause(100)
+        }
+        Y += 1
+    }
+    for (let X = 0; X <= 4; X++) {
+        led.plot(X, Y)
+        basic.pause(100)
+    }
+    X = 0
+    Y = 0
+    for (let index = 0; index < 2; index++) {
+        for (let X = 0; X <= 4; X++) {
+            led.unplot(X, Y)
+            basic.pause(100)
+        }
+        Y += 1
+        for (let X = 0; X <= 4; X++) {
+            led.unplot(4 - X, Y)
+            basic.pause(100)
+        }
+        Y += 1
+    }
+    for (let X = 0; X <= 4; X++) {
+        led.unplot(X, Y)
+        basic.pause(100)
+    }
 })
