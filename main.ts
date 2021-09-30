e5199a8ec55a23bc7f58d364220af03892ed3e1c
318511 <91246969+318511@users.noreply.github.com>
2021-09-30 16:24:24 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,8 @@
-for (let index = 0; index <= 4; index++) {
+let X = 0
+let Y = 0
+for (let index = 0; index < 2; index++) {
 	
 }
 basic.forever(function () {
-    basic.showLeds(`
-        # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . # . #
-        . # . # .
-        # . # . .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . # . .
-        . # . # .
-        # . # . #
-        . # . # .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . . . # .
-        # . # . .
-        . # . # .
-        . . # . #
-        . # . . .
-        `)
+	
 })
